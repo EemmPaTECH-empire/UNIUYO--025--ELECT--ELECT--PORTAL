1a19b25bac0b4d59f3c22571d86ca0bb58cbf59e
EemmPaTECH-empire <EemmPaTECH@gmail.com>
2026-02-21 17:05:14 +0100

    Create js/timetable.js

---
+++ js/timetable.js
@@ -0,0 +1,26 @@
+const timetable = {
+  Monday:[
+    {start:"08:00",end:"10:00",course:"GST 111"},
+    {start:"10:00",end:"12:00",course:"PHYSICS 111"},
+    {start:"13:00",end:"17:00",course:"PHYSICS 117"}
+  ],
+  Tuesday:[
+    {start:"08:00",end:"10:00",course:"EEE 111"},
+    {start:"10:00",end:"12:00",course:"MATH 111"},
+    {start:"12:00",end:"14:00",course:"EEE PRACTICAL"},
+    {start:"15:00",end:"17:00",course:"GET 111"}
+  ],
+  Wednesday:[
+    {start:"08:00",end:"10:00",course:"PHYSICS 112"},
+    {start:"10:00",end:"12:00",course:"CHEMISTRY 111"}
+  ],
+  Thursday:[
+    {start:"10:00",end:"13:00",course:"CHEMISTRY 117"},
+    {start:"15:00",end:"17:00",course:"GET 111"}
+  ],
+  Friday:[
+    {start:"08:00",end:"10:00",course:"MATH 111"},
+    {start:"11:00",end:"13:00",course:"GET 111"},
+    {start:"15:00",end:"17:00",course:"GET 111"}
+  ]
+};
